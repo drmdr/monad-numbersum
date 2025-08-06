4a00b9fff58658ed267ddac2be07958eb0a91c28
drmdr <dorimix.exe@gmail.com>
2025-08-06 16:15:14 +0900

    fix: Hardcode correct production URL for number-sum

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,13 +1,4 @@
 export const MESSAGE_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 30; // 30 day
 
-const APP_URL = process.env.NEXT_PUBLIC_URL
-  ? process.env.NEXT_PUBLIC_URL
-  : process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : null;
-
-if (!APP_URL) {
-  throw new Error('NEXT_PUBLIC_URL or NEXT_PUBLIC_VERCEL_URL is not set');
-}
-
-export { APP_URL };
+// 正しい本番URLを直接指定
+export const APP_URL = 'https://monad-numbersum.vercel.app/';
